41ff72d8608d69d627c2df6567bb799acd3196be
A1exandre Pradignac <pa230262@etu.univ-lehavre.fr>
2024-10-18 08:44:11 +0200

    Ajout drop extractor

--- src/main/java/net/A1exPrdgc/biotechmod/block/custom/Extractor.java
+++ src/main/java/net/A1exPrdgc/biotechmod/block/custom/Extractor.java
@@ -3,12 +3,16 @@ package net.A1exPrdgc.biotechmod.block.custom;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.HorizontalBlock;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.state.StateContainer;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.common.extensions.IForgeTileEntity;
 
 import javax.annotation.Nullable;
 
-public class Extractor extends HorizontalBlock{
+public class Extractor extends HorizontalBlock {
 	public Extractor(Properties builder){
 		super(builder);
 	}
